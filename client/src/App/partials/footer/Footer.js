779f1b6720b134bab1baf29e339d4ba98578640f
jensderyckere <jensdery1@student.arteveldehs.be>
2021-05-01 11:23:48 +0200

    final check

--- client/src/App/partials/footer/Footer.js
+++ client/src/App/partials/footer/Footer.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 // Images
 import Logo from '../../assets/logos/full-logo.svg';
+import ArteveldeLogo from '../../assets/logos/artevelde.png';
 
 export const Footer = () => {
   return (
-    <footer className="footer d-flex justify-content-center justify-content-md-start">
+    <footer className="footer d-flex justify-content-between">
       <img src={Logo} alt="logo" />
+      <img className="artevelde-logo" src={ArteveldeLogo} alt="artevelde" />
     </footer>
   );
 };
